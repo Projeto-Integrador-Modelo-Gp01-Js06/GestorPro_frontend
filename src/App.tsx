7b825b793b5e60200e8485749b91436b61619824
ustavoteles <telesgustavo.dev@gmail.com>
2025-02-26 00:20:34 -0300

    :sparkles: feat: Adicionando pagina Sobre

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import Sobre from "./pages/sobre/Sobre";
 
 
 
@@ -13,10 +14,11 @@ function App() {
        <BrowserRouter>
        <Navbar />
         <div>
-          <div className="flex-grow">
+          <div className="min-h-[75vh]">
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/sobre" element={<Sobre />} />
           </Routes>
           </div>
         </div>
